d07eead4eff636e95bbd63dcc1c10ada1cdf5d2a
CodingDonky <saintdane75@gmail.com>
2019-06-20 15:45:18 -0700

    updated npc generator

--- javascript/npc_generator.js
+++ javascript/npc_generator.js
@@ -1,6 +1,6 @@
 // document.getElementById('date').innerHTML = new Date().toDateString();
 
-// READING JSON FILE
+//////////////////// READING JSON FILE ////////////////////
 function readTextFile(file, callback) {
   var rawFile = new XMLHttpRequest();
   rawFile.overrideMimeType("application/json");
@@ -13,39 +13,19 @@ function readTextFile(file, callback) {
   rawFile.send(null);
 }
 
+var npc_json = null;
+
 readTextFile(
   "https://raw.githubusercontent.com/CodingDonky/CodingDonky.github.io/\
 master/json/npc_generation_options.json", function(text){
-  var npc_data = JSON.parse(text);
-  // console.log(Object.keys(npc_data))
-  // console.log(npc_data.dog)
-  // console.log(npc_data);
+  npc_json = JSON.parse(text);
+  // console.log(Object.keys(npc_json))
+  // console.log(npc_json.dog)
+  // console.log(npc_json);
 });
-// READING JSON FILE
+//////////////////// READING JSON FILE ////////////////////
 
-function getRandomListElement( data_list ) {
-  var list_element = data_list[ Math.floor(Math.random()*data_list.length) ];
-  return list_element;
-}
-
-function mouseDown_generateNPC(obj){
-  print_contents = "";
-
-  // generate combat-focused NPC
-  if( document.getElementById("npc_type_radio_combat").checked ){
-    print_contents += Object.keys(npc_data)[0];
-  }// generate town-focused NPC
-  else if( document.getElementById("npc_type_radio_town").checked ){
-    print_contents += Object.keys(npc_data)[0];
-  }// generate exploration-focused NPC
-  else if( document.getElementById("npc_type_radio_explore").checked ){
-    print_contents += Object.keys(npc_data)[0];
-  }
-
-  document.getElementById('print_output').innerHTML = print_contents;
-}
-
-
+//////////////////// NPC CLASS DEFINITION ////////////////////
 class NPC {
     constructor(name) {
       this.name = name;
@@ -64,46 +44,126 @@ class NPC {
   }
   
 const npc = new NPC("[REDACTED]");
-// console.log(npc.area);
+//////////////////// NPC CLASS DEFINITION ////////////////////
 
-//////////////////// NAME BUTTON STUFF ////////////////////
-function mouseDown_name(obj){
-  // set random name
-  random_name = getRandomName()
-  changeName( random_name )
-  // obj.style.backgroundColor = "#1ec5e5";
-  // obj.innerHTML = "Release Me";
-}function mouseUp_name(obj) {
-  // obj.style.backgroundColor = "#D94A38";
-  // obj.innerHTML = "Generate Random Name";
+
+function getRandomListElement( data_list ) {
+  var list_element = data_list[ Math.floor(Math.random()*data_list.length) ];
+  return list_element;
 }
 
-function getRandomName(){
-  var name_list = Array('bob','dood','scarface','shia');
-  var name = name_list[ Math.floor(Math.random()*name_list.length) ];
+//////////////////// MOUSEDOWN for NPC GENERATOR BUTTON ////////////////////
+function mouseDown_generateNPC(obj){
+  print_contents = "";
+  // Object.keys(npc_json)
 
-  return name
+  var c = npc_json.combat-focus
+  console.log( c );
+  console.log( c.classes );
+  // console.log( c.classes[0] );
+
+  name = getRandomListElement( npc_json.names )
+  print_contents += 'name: ' + name + '<br>';
+  npc.name = name
+
+  gender = getRandomListElement( npc_json.genders )
+  print_contents += 'gender: ' + gender + '<br>';
+
+  mannerism = getRandomListElement( npc_json.mannerisms )
+  print_contents += 'mannerism: ' + mannerism + '<br>';
+
+  quirk = getRandomListElement( npc_json.quirks )
+  print_contents += 'quirk: ' + quirk + '<br>';
+
+  race = getRandomListElement( npc_json.races )
+  print_contents += 'race: ' + race + '<br>';
+
+  weakness = getRandomListElement( npc_json.weaknesses )
+  print_contents += 'weakness: ' + weakness + '<br>';
+
+  strength = getRandomListElement( npc_json.strengths )
+  print_contents += 'strength: ' + strength + '<br>';
+
+
+  // generate combat-focused NPC
+  if( document.getElementById("npc_type_radio_combat").checked ){
+    print_contents = generate_combat_npc( print_contents );
+  }// generate town-focused NPC
+  else if( document.getElementById("npc_type_radio_town").checked ){
+    print_contents = generate_town_npc( print_contents );
+  }// generate exploration-focused NPC
+  else if( document.getElementById("npc_type_radio_explore").checked ){
+    print_contents = generate_exploration_npc( print_contents );
+  }
+
+  document.getElementById('print_output').innerHTML = print_contents;
 }
 
-function changeName( name ) {
-  document.getElementById("name_field").value = name;
-  npc.name = name;
-}
-//////////////////// NAME BUTTON STUFF ////////////////////
+function generate_combat_npc( print_contents ) {
+  combat_class = getRandomListElement( npc_json.combat-focus.classes );
+  print_contents += 'class: ' + combat_class + '<br>';
 
-//////////////////// OCCUPATION BUTTON STUFF ////////////////////
-function mouseDown_occupation(obj){
-  random_occupation = getRandomOccupation()
-  changeOccupation( random_occupation )
-}
-function getRandomOccupation(){
-  var occ_list = Array('Smith','Guard','Shopkeep','Farmer');
-  var occ = occ_list[ Math.floor(Math.random()*occ_list.length) ];
+  posse = getRandomListElement( npc_json.combat-focus.posse );
+  print_contents += 'posse: ' + posse + '<br>';
 
-  return occ
+  ability = getRandomListElement( npc_json.combat-focus.abilities );
+  print_contents += 'ability: ' + ability + '<br>';
+
+  combat_style = getRandomListElement( npc_json.combat-focus.combat_styles );
+  print_contents += 'combat_style: ' + combat_style + '<br>';
+
+  loot = getRandomListElement( npc_json.combat-focus.loot );
+  print_contents += 'loot: ' + loot + '<br>';
+
+  long_term_goal = getRandomListElement( npc_json.combat-focus.long_term_goals );
+  print_contents += 'long_term_goal: ' + long_term_goal + '<br>';
+
+  post_long_term_goal = getRandomListElement( 
+    npc_json.combat-focus.post_long_term_goals );
+  print_contents += 'post_long_term_goal: ' + post_long_term_goal + '<br>';
+
+  return print_contents
 }
-function changeOccupation( occ ) {
-  document.getElementById("occupation_field").value = occ;
-  npc.occupation = occ;
+
+function generate_town_npc( print_contents ) {
+  job = getRandomListElement( npc_json.city-focus.jobs );
+  print_contents += 'job: ' + job + '<br>';
+
+  skill_level = getRandomListElement( npc_json.city-focus.skill_levels );
+  print_contents += 'skill_level: ' + skill_level + '<br>';
+  
+  trustworthiness = getRandomListElement( npc_json.city-focus.trustworthiness );
+  print_contents += 'trustworthiness: ' + trustworthiness + '<br>';
+
+  friendliness = getRandomListElement( npc_json.city-focus.friendliness );
+  print_contents += 'friendliness: ' + friendliness + '<br>';
+
+  house_material = getRandomListElement( npc_json.city-focus.house_material );
+  house_type = getRandomListElement( npc_json.city-focus.house_type );
+  house_adj = getRandomListElement( npc_json.city-focus.house_adj );
+  house_description = house_adj + ' ' + house_material + ' ' + house_adj;
+  print_contents += 'domicile: ' + house_description + '<br>';
+
+  nobility_status = getRandomListElement( npc_json.city-focus.nobility_status );
+  print_contents += 'nobility_status: ' + nobility_status + '<br>';
+
+  pet = getRandomListElement( npc_json.city-focus.pets );
+  print_contents += 'pet: ' + pet + '<br>';
+
+  return print_contents
 }
-//////////////////// OCCUPATION BUTTON STUFF ////////////////////
+
+function generate_exploration_npc( print_contents ) {
+  sleep_location = getRandomListElement( npc_json.exploration-focus.sleep_locations );
+  print_contents += 'sleep_location: ' + sleep_location + '<br>';
+  
+  motivation = getRandomListElement( npc_json.exploration-focus.motivation );
+  print_contents += 'motivation: ' + motivation + '<br>';
+
+  free_time_activity = getRandomListElement( 
+    npc_json.exploration-focus.free_time_activities );
+  print_contents += 'free_time_activity: ' + free_time_activity + '<br>';
+
+  return print_contents
+}
+//////////////////// MOUSEDOWN for NPC GENERATOR BUTTON ////////////////////
